9668433fd6cef9444972ef41025c3bd0a310b14c
Dang Van Hung <hung14052001@gmail.com>
2022-05-12 13:09:18 +0700

    Add project files.

---
+++ QLChamCong/fDangNhap.Designer.cs
@@ -0,0 +1,223 @@
+﻿
+namespace QLChamCong
+{
+    partial class fDangNhap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fDangNhap));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.ctButton1 = new WindowsFormsApp1.ctButton();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.ctTextBox2 = new QLChamCong.Control.ctTextBox();
+            this.ctTextBox1 = new QLChamCong.Control.ctTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.checkBox1);
+            this.panel1.Controls.Add(this.ctButton1);
+            this.panel1.Controls.Add(this.pictureBox4);
+            this.panel1.Controls.Add(this.pictureBox3);
+            this.panel1.Controls.Add(this.ctTextBox2);
+            this.panel1.Controls.Add(this.ctTextBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Location = new System.Drawing.Point(200, 126);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(700, 300);
+            this.panel1.TabIndex = 1;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(386, 256);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(208, 21);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "Đăng nhập với quyền Admin";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // ctButton1
+            // 
+            this.ctButton1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(47)))), ((int)(((byte)(94)))), ((int)(((byte)(124)))));
+            this.ctButton1.BackGroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(47)))), ((int)(((byte)(94)))), ((int)(((byte)(124)))));
+            this.ctButton1.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.ctButton1.BorderRadius = 40;
+            this.ctButton1.BorderSize = 0;
+            this.ctButton1.FlatAppearance.BorderSize = 0;
+            this.ctButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ctButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ctButton1.ForeColor = System.Drawing.Color.White;
+            this.ctButton1.Location = new System.Drawing.Point(371, 210);
+            this.ctButton1.Name = "ctButton1";
+            this.ctButton1.Size = new System.Drawing.Size(234, 40);
+            this.ctButton1.TabIndex = 5;
+            this.ctButton1.Text = "Login";
+            this.ctButton1.TextColor = System.Drawing.Color.White;
+            this.ctButton1.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::QLChamCong.Properties.Resources.Security_Key_Security_icon;
+            this.pictureBox4.Location = new System.Drawing.Point(327, 161);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(38, 35);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 4;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::QLChamCong.Properties.Resources.icons8_user_64;
+            this.pictureBox3.Location = new System.Drawing.Point(327, 105);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(38, 35);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // ctTextBox2
+            // 
+            this.ctTextBox2.BackColor = System.Drawing.SystemColors.Window;
+            this.ctTextBox2.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(6)))), ((int)(((byte)(18)))), ((int)(((byte)(75)))));
+            this.ctTextBox2.BorderFocusColor = System.Drawing.Color.HotPink;
+            this.ctTextBox2.BorderRadius = 0;
+            this.ctTextBox2.BorderSize = 3;
+            this.ctTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ctTextBox2.ForeColor = System.Drawing.Color.Black;
+            this.ctTextBox2.Location = new System.Drawing.Point(371, 161);
+            this.ctTextBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.ctTextBox2.Multiline = false;
+            this.ctTextBox2.Name = "ctTextBox2";
+            this.ctTextBox2.Padding = new System.Windows.Forms.Padding(12, 7, 12, 7);
+            this.ctTextBox2.PasswordChar = true;
+            this.ctTextBox2.Size = new System.Drawing.Size(277, 35);
+            this.ctTextBox2.TabIndex = 3;
+            this.ctTextBox2.Texts = "";
+            this.ctTextBox2.UnderlinedStyle = true;
+            // 
+            // ctTextBox1
+            // 
+            this.ctTextBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.ctTextBox1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(6)))), ((int)(((byte)(18)))), ((int)(((byte)(75)))));
+            this.ctTextBox1.BorderFocusColor = System.Drawing.Color.HotPink;
+            this.ctTextBox1.BorderRadius = 0;
+            this.ctTextBox1.BorderSize = 3;
+            this.ctTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ctTextBox1.ForeColor = System.Drawing.Color.Black;
+            this.ctTextBox1.Location = new System.Drawing.Point(371, 105);
+            this.ctTextBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.ctTextBox1.Multiline = false;
+            this.ctTextBox1.Name = "ctTextBox1";
+            this.ctTextBox1.Padding = new System.Windows.Forms.Padding(12, 7, 12, 7);
+            this.ctTextBox1.PasswordChar = false;
+            this.ctTextBox1.Size = new System.Drawing.Size(277, 35);
+            this.ctTextBox1.TabIndex = 2;
+            this.ctTextBox1.Texts = "";
+            this.ctTextBox1.UnderlinedStyle = true;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Arial Black", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(326, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(322, 46);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Member Login";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox2.Image = global::QLChamCong.Properties.Resources.icons8_login_64;
+            this.pictureBox2.Location = new System.Drawing.Point(34, 50);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(220, 200);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::QLChamCong.Properties.Resources.Login_BG;
+            this.pictureBox1.Location = new System.Drawing.Point(-1, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1082, 553);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // fDangNhap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1082, 553);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "fDangNhap";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Đăng Nhập";
+            this.Load += new System.EventHandler(this.fDangNhap_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private Control.ctTextBox ctTextBox2;
+        private Control.ctTextBox ctTextBox1;
+        private System.Windows.Forms.Label label1;
+        private WindowsFormsApp1.ctButton ctButton1;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ QLChamCong/fDangNhap.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLChamCong
+{
+    public partial class fDangNhap : Form
+    {
+        public fDangNhap()
+        {
+            InitializeComponent();
+        }
+
+        private void fDangNhap_Load(object sender, EventArgs e)
+        {
+            //FormBorderStyle = FormBorderStyle.FixedSingle;
+        }
+    }
+}
